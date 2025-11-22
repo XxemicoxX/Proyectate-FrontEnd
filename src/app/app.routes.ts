983a2e49e4958b55873abec6d50a05dc3dca33c6
chaufalover <tacuresancheztomas@gmail.com>
2025-11-22 18:04:20 -0500

    sistemas de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './features/public/inicio/inicio';
+import { NoEncontrado } from './features/public/no-encontrado/no-encontrado';
+import { Nosotros } from './features/public/nosotros/nosotros';
+import { Contacto } from './features/public/contacto/contacto';
+import { Login } from './features/auth/login/login';
+import { Registro } from './features/auth/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: Inicio, title: 'Inicio | Proyectate'},
+    {
+        path: 'nosotros', component: Nosotros, title: 'Nosotros | Proyectate'
+    },
+    {
+        path: 'contacto', component: Contacto, title: 'Contacto | Proyectate'
+    },
+    {
+        path: 'inicio-sesion', component: Login, title: 'Iniciar Sesión | Proyectate'
+    },
+    {
+        path: 'registro', component: Registro, title: 'Registro | Proyectate'
+    },
+    {
+        path: '**', component: NoEncontrado, title: 'Página no encontrada'
+    }
+];
--- src/app/features/public/no-encontrado/no-encontrado.ts
+++ src/app/features/public/no-encontrado/no-encontrado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-no-encontrado',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './no-encontrado.html',
   styleUrl: './no-encontrado.scss'
 })
