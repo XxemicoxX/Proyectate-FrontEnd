d7e1ee49020c499f0911631e78ff1169c4923914
chaufalover <tacuresancheztomas@gmail.com>
2025-11-22 19:38:12 -0500

    Contacto mediante formulario basado en plantillas y creacion de componente user/proyecto

--- src/app/features/public/contacto/contacto.ts
+++ src/app/features/public/contacto/contacto.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, model } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './contacto.html',
   styleUrl: './contacto.scss'
 })
 export class Contacto {
+
+  model = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+  };
+
+   enviarConsulta (){
+    console.log(this.model);
+  }
 
 }
